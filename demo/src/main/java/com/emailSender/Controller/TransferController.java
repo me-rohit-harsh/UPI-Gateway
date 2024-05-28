f7df0c88b121826da583cbff0e3cd08ddcaa2209
Rohit kumar <rohitkumarah369@gmail.com>
2024-05-28 15:59:09 +0530

    new update

--- demo/src/main/java/com/emailSender/Controller/TransferController.java
+++ demo/src/main/java/com/emailSender/Controller/TransferController.java
@@ -1,12 +1,17 @@
 package com.emailSender.Controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.emailSender.Repository.TransactionRepository;
 import com.emailSender.Repository.UserRepository;
+import com.emailSender.Service.EmailService;
+import com.emailSender.model.Transaction;
 import com.emailSender.model.User;
 
 import jakarta.servlet.http.HttpSession;
@@ -15,6 +20,10 @@ import jakarta.servlet.http.HttpSession;
 public class TransferController {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private TransactionRepository transactionRepository;
+	@Autowired
+	private EmailService emailService;
 
 	@PostMapping("/findbeneficiary")
 	public String findBeneficiary(@RequestParam("userId") String userId, HttpSession session,
@@ -34,30 +43,83 @@ public class TransferController {
 
 	@PostMapping("/transferMoney")
 	public String transferMoney(@RequestParam("balance") double balance, @RequestParam("username") String username,
-	                            RedirectAttributes redirectAttributes, HttpSession session) {
-	    User beneficiaryUser = userRepository.findByUsername(username);
-	    User user = userRepository.findById((Long) session.getAttribute("userId")).orElse(null);
+			RedirectAttributes redirectAttributes, HttpSession session) {
+		User beneficiaryUser = userRepository.findByUsername(username);
+		User user = userRepository.findById((Long) session.getAttribute("userId")).orElse(null);
 
-	    if (beneficiaryUser != null && user != null) {
-	        if (user.getBalance() >= balance) {
-	            // Deduct balance from the user
-	            user.setBalance(user.getBalance() - balance);
-	            // Add balance to the beneficiary
-	            beneficiaryUser.setBalance(beneficiaryUser.getBalance() + balance);
+		if (beneficiaryUser != null && user != null) {
+			if (user.getBalance() >= balance) {
+				// Deduct balance from the user
+				user.setBalance(user.getBalance() - balance);
+				// Add balance to the beneficiary
+				beneficiaryUser.setBalance(beneficiaryUser.getBalance() + balance);
 
-	            // Save the updated users
-	            userRepository.save(user);
-	            userRepository.save(beneficiaryUser);
+				// Save the updated users
+				userRepository.save(user);
+				userRepository.save(beneficiaryUser);
+				// Create a debit transaction for the user
+				Transaction userTransaction = new Transaction();
+				userTransaction.setUser(user);
+				userTransaction.setAmount(balance);
+				userTransaction.setType("Debit");
+				userTransaction.setStatus(true);
+				userTransaction.setRefId(UUID.randomUUID().toString().replace("-", "").substring(0, 8));
+				userTransaction.setMethod("Wallet Transfer");
 
-	            redirectAttributes.addFlashAttribute("message", "Amount Transferred successfully!!");
-	        } else {
-	            redirectAttributes.addFlashAttribute("errorMsg", "Insufficient balance to transfer the amount!");
-	        }
-	    } else {
-	        redirectAttributes.addFlashAttribute("errorMsg", "Beneficiary Not Found!");
-	    }
+				// Create a credit transaction for the beneficiary
+				Transaction beneficiaryTransaction = new Transaction();
+				beneficiaryTransaction.setRefId(UUID.randomUUID().toString().replace("-", "").substring(0, 8));
+				beneficiaryTransaction.setUser(beneficiaryUser);
+				beneficiaryTransaction.setAmount(balance);
+				beneficiaryTransaction.setType("Credit");
+				beneficiaryTransaction.setStatus(true);
+				beneficiaryTransaction.setMethod("Wallet Transfer");
 
-	    return "redirect:/p2p";
+				// Save the transactions
+				transactionRepository.save(userTransaction);
+				transactionRepository.save(beneficiaryTransaction);
+				String userEmailBody = String.format(
+						"Dear %s,\n\n" +
+								"We are pleased to inform you that your recent payment has been successfully processed.\n\n"
+								+
+								"Details of the transaction are as follows:\n" +
+								"Amount: %.2f\n" +
+								"To: %s\n" +
+								"Date and Time: %s\n" +
+								"Payment Method: %s\n\n" +
+								"Thank you for using our services!\n\n" +
+								"Best regards,\n" +
+								"JixWallet",
+						user.getUsername(), balance,
+						username,
+						userTransaction.getCreatedAt().toString(), userTransaction.getMethod());
+
+				String benEmailBody = String.format(
+						"Dear %s,\n\n" +
+								"We are pleased to inform you that you have received a payment.\n\n" +
+								"Details of the transaction are as follows:\n" +
+								"Amount: %.2f\n" +
+								"From: %s\n" +
+								"Date and Time: %s\n" +
+								"Payment Method: %s\n\n" +
+								"Thank you for using our services!\n\n" +
+								"Best regards,\n" +
+								"JixWallet",
+						username, balance, user.getUsername(),
+						beneficiaryTransaction.getCreatedAt().toString(), beneficiaryTransaction.getMethod());
+
+				emailService.sendSimpleEmail(user.getEmail(), "Transaction Successful - JixWallet", userEmailBody);
+				emailService.sendSimpleEmail(beneficiaryUser.getEmail(), "Credit Alert - JixWallet", benEmailBody);
+
+				redirectAttributes.addFlashAttribute("message", "Amount Transferred successfully!!");
+			} else {
+				redirectAttributes.addFlashAttribute("errorMsg", "Insufficient balance to transfer the amount!");
+			}
+		} else {
+			redirectAttributes.addFlashAttribute("errorMsg", "Beneficiary Not Found!");
+		}
+
+		return "redirect:/p2p";
 	}
 
 }
